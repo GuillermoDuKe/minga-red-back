e31f1e5edb50b76f727de61fc452e6f9585b0b10
Guillermo Duque <guille980627@gmail.com>
2023-05-24 17:09:27 -0500

    comments added

--- server.js
+++ server.js
@@ -4,12 +4,13 @@ import "./config/database.js"
 
 const server = express()       // Definimos un servidor directamente ejecutando el módulo como si fuera una función
 const PORT = process.env.PORT || 8080              // Definimos el puerto local donde va a funcionar el servidor.
-
 const ready = ()=> console.log("Ready on: "+PORT) // Definimos la función que se va a ejecutar cuando se levante el servidor.
 
+// middleware 
 server.use(express.json())
 server.use(express.urlencoded({ extended: false }))
 
+// ROUTER + server
 server.listen(PORT, ready)     // Ejecutamos el método listen para poder inicializar el servidor. La callback “ready” es opcional pero nos ayudará a saber cuando el servidor esté funcionando.
 
 // console.log(process.env.NODE_ENV)
