46c18fb55693bb5667b937cc1c0fecf235ac9403
Guillermo Duque <guille980627@gmail.com>
2023-05-26 09:08:35 -0500

    update

--- router/chapters.js
+++ router/chapters.js
@@ -1,10 +1,8 @@
 import { Router } from "express"
-import create from "../controllers/authors/create.js"
 import read from "../controllers/chapters/read.js"
 
 let chaptersRouter = Router()
 
 chaptersRouter.get('/', read)
-// authorsRouter.post('/',create)
 
 export default chaptersRouter
